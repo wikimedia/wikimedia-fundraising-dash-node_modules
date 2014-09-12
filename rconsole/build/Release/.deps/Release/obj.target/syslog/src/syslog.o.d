84b47915f8ba21317ce0f7f682510288da1e8eb6
Sherah Smith <ssmith@wikimedia.org>
2014-09-12 14:33:31 -0700

    first commit

---
+++ rconsole/build/Release/.deps/Release/obj.target/syslog/src/syslog.o.d
@@ -0,0 +1,19 @@
+cmd_Release/obj.target/syslog/src/syslog.o := c++ '-D_DARWIN_USE_64_BIT_INODE=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/Users/sherahsmith/.node-gyp/0.10.29/src -I/Users/sherahsmith/.node-gyp/0.10.29/deps/uv/include -I/Users/sherahsmith/.node-gyp/0.10.29/deps/v8/include  -Os -gdwarf-2 -mmacosx-version-min=10.5 -arch x86_64 -Wall -Wendif-labels -W -Wno-unused-parameter -fno-rtti -fno-exceptions -fno-threadsafe-statics -fno-strict-aliasing -MMD -MF ./Release/.deps/Release/obj.target/syslog/src/syslog.o.d.raw  -c -o Release/obj.target/syslog/src/syslog.o ../src/syslog.cc
+Release/obj.target/syslog/src/syslog.o: ../src/syslog.cc \
+  /Users/sherahsmith/.node-gyp/0.10.29/deps/v8/include/v8.h \
+  /Users/sherahsmith/.node-gyp/0.10.29/deps/v8/include/v8stdint.h \
+  /Users/sherahsmith/.node-gyp/0.10.29/src/node.h \
+  /Users/sherahsmith/.node-gyp/0.10.29/deps/uv/include/uv.h \
+  /Users/sherahsmith/.node-gyp/0.10.29/deps/uv/include/uv-private/uv-unix.h \
+  /Users/sherahsmith/.node-gyp/0.10.29/deps/uv/include/uv-private/ngx-queue.h \
+  /Users/sherahsmith/.node-gyp/0.10.29/deps/uv/include/uv-private/uv-darwin.h \
+  /Users/sherahsmith/.node-gyp/0.10.29/src/node_object_wrap.h
+../src/syslog.cc:
+/Users/sherahsmith/.node-gyp/0.10.29/deps/v8/include/v8.h:
+/Users/sherahsmith/.node-gyp/0.10.29/deps/v8/include/v8stdint.h:
+/Users/sherahsmith/.node-gyp/0.10.29/src/node.h:
+/Users/sherahsmith/.node-gyp/0.10.29/deps/uv/include/uv.h:
+/Users/sherahsmith/.node-gyp/0.10.29/deps/uv/include/uv-private/uv-unix.h:
+/Users/sherahsmith/.node-gyp/0.10.29/deps/uv/include/uv-private/ngx-queue.h:
+/Users/sherahsmith/.node-gyp/0.10.29/deps/uv/include/uv-private/uv-darwin.h:
+/Users/sherahsmith/.node-gyp/0.10.29/src/node_object_wrap.h:
